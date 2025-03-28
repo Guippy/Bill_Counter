f8a54dad788dd3cf8ce31aa694247edb60eeffea
Guippy <guipecar@gmail.com>
2025-03-28 07:57:46 -0300

    Working

--- Bills.c
+++ Bills.c
@@ -7,9 +7,25 @@ void CountBills(int);
 
 int main(void){
 int ScannerValue;
-printf("enter a value\n-->");
-scanf("%d", &ScannerValue);
-CountBills(ScannerValue);
+int Option;
+char Command[] = "cls";
+while(1){
+printf("\nCount bills(1)\nClear(2)\nExit(3)\n");
+scanf("%d", &Option);
+switch(Option){
+    case 1:
+        printf("enter a value\n-->");
+        scanf("%d", &ScannerValue);
+        CountBills(ScannerValue);
+        break;
+    case 2:
+        system(Command);
+        break;
+    case 3:
+        exit(0);
+        break;
+}
+}
 return 0;
 }
 
@@ -17,7 +33,6 @@ return 0;
 
 void CountBills(int Value){
 // 20 10 5 2
-//20 dollar Bill
 int X = 20;
 while (1){
 int ValueNew = (Value/X);
